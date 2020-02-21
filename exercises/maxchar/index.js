b78243bfb5a3d2764fb2a69c1f9105b1ae83bbc4
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-02-20 21:40:42 -0500

    maxchar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,41 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var chars = {};
+    for(let char of str)
+    {
+        if(chars[char] === undefined){
+            chars[char] = 1;
+        }
+        else{
+            chars[char] += 1;
+        }
+    }
+    console.log(chars);
+   var maxCharCount = 0;
+   var maxChar = '';
+    for(let attr in chars){
+        if(chars[attr] > maxCharCount){
+            maxChar = attr;
+            console.log(attr);
+            maxCharCount = chars[attr];
+            console.log(maxCharCount);
+            
+        }
+    }
+    
+//    var charsAttr = Object.keys(chars);
+//    var maxCharCount = 0;
+//    var maxChar = '';
+//    for(char of charsAttr){
+//         if( chars[char] > maxCharCount){
+//             maxChar = char;
+//             maxCharCount = chars[char];
+//         }
+//    }
+
+   return maxChar;
+}
 
 module.exports = maxChar;
