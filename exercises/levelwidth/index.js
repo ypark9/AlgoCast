3214b1de3ce94ca8d0ac7b5019bb5b6193c125a7
Park <ypark9@ncsu.edu>
2020-02-26 16:21:54 -0500

    Tree level and width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,29 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let retArr = [0];
+    let arr = [root, 's'];
+    let level = 0; //
+
+    while (arr.length > 1) {
+        let node = arr.shift;
+        if(node === 's')
+        {
+            retArr.push(0);
+            level ++;
+            arr.push('s');
+            continue;
+        }
+        else
+            retArr[level] ++; //retArr[retArr.length-1] ++;
+
+        for (let child of node.children) {
+            arr.push(child);
+        }
+    }
+
+    return retArr;
+}
 
 module.exports = levelWidth;
