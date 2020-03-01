84edaaf28f867c2e8c071eac3207927c1a10748b
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-02-29 19:42:36 -0500

    update Event implementation

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,32 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.event = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(this.event[eventName] != undefined){
+      this.event[eventName].push(callback);
+    }
+    else {
+      this.event[eventName] = [callback]; //not = callback;
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    for(let cb of this.event[eventName]){
+      cb();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.event[eventName] =  []; //not setting null since trigger gets problem
+  }
 }
 
 module.exports = Events;
