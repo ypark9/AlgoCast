b902a8cd4d35bab0d50d056e7980cde2ec62f39c
Yoonsoo Park <ypark9@ncsu.edu>
2020-04-03 14:51:44 -0400

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // The idea is when you see the circulation see previous value again. 
 // This means it never be happy number like... 
 // if you see the same problem over and over, there is a problem to be happy in your life. Fix it.
-class Solution {
+class HappyNumberSolution {
 public:
   int SumDigits(int num) {
     int sum = 0;
@@ -25,4 +25,22 @@ public:
 
       return num == 1;
     }
+};
+
+
+//DP
+class MaxSubArraySolution {
+public:
+int maxSubArray(vector<int>& nums) {
+        int sum = 0, smax = INT_MIN;
+        for (int num : nums) {
+            sum += num;
+            smax = max(smax, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return smax;
+    }
+    
 };
