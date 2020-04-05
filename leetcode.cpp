6a2c0eae88115cbb16fa0b68a47ede30796dd03c
Yoonsoo Park <ypark9@ncsu.edu>
2020-04-05 12:45:33 -0400

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,5 +1,7 @@
 #include <set>
 using namespace std;
+
+//# Happy number
 // The idea is when you see the circulation see previous value again. 
 // This means it never be happy number like... 
 // if you see the same problem over and over, there is a problem to be happy in your life. Fix it.
@@ -27,8 +29,9 @@ public:
     }
 };
 
-
-//DP
+//# Max subarray
+//
+// DP
 class MaxSubArraySolution {
 public:
 int maxSubArray(vector<int>& nums) {
@@ -41,6 +44,28 @@ int maxSubArray(vector<int>& nums) {
             }
         }
         return smax;
-    }
+    }    
+};
+
+
+// # Maximum profit 2
+// Check how to interpret the idea of selling and buying 
+// to get a difference whenever the next value is bigger than previous.
+class maxProfitSolution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int total = 0;
+        if(prices.size() == 0)
+            return total;
+        for (int i = 0; i < prices.size() - 1; i++) 
+        {
+            if(i+1 <= prices.size() - 1)
+            {
+                if (prices[i+1] > prices[i]) 
+                    total += prices[i+1] - prices[i];
+            }
+        }
     
+        return total;
+    }
 };
