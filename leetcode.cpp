ea9da870c28c918c39732f05714ede08240a78e3
Yoonsoo Park <ypark9@ncsu.edu>
2020-04-10 02:01:36 -0400

    change the format of backspaceCompare solution to better reading.

--- leetcode.cpp
+++ leetcode.cpp
@@ -195,8 +195,14 @@ class BackspaceCompareSolution {
 public:
     bool backspaceCompare(string S, string T) {
         string a = "", b = "";
-        for(auto c: S) c == '#' ? a.size() > 0 ? a.pop_back() : void() : a.push_back(c);
-        for(auto c: T) c == '#' ? b.size() > 0 ? b.pop_back() : void() : b.push_back(c);
+        for(auto c: S)
+        {
+            c == '#' ? (a.size() > 0 ? a.pop_back() : void()) : a.push_back(c);
+        } 
+        for(auto c: T) 
+        {
+            c == '#' ? (b.size() > 0 ? b.pop_back() : void()) : b.push_back(c);
+        }
         return a == b;
     }
 };
