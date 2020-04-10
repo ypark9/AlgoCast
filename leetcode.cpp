de94ece2acc2208e54dfc90b334f21bd076d065e
Yoonsoo Park <ypark9@ncsu.edu>
2020-04-10 01:57:59 -0400

    BackspaceCompare using Stack.

--- leetcode.cpp
+++ leetcode.cpp
@@ -184,3 +184,19 @@ public:
         return slow;
     }
 };
+
+
+/*
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+*/
+class BackspaceCompareSolution {
+public:
+    bool backspaceCompare(string S, string T) {
+        string a = "", b = "";
+        for(auto c: S) c == '#' ? a.size() > 0 ? a.pop_back() : void() : a.push_back(c);
+        for(auto c: T) c == '#' ? b.size() > 0 ? b.pop_back() : void() : b.push_back(c);
+        return a == b;
+    }
+};
