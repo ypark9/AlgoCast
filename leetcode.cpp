3b3e41bf75ae2fc9675e97ffd30f73509c1f4963
Yoonsoo Park <ypark9@ncsu.edu>
2020-04-17 15:53:42 -0400

    numIslands (BFS)

--- leetcode.cpp
+++ leetcode.cpp
@@ -272,7 +272,7 @@ Set the element to equal '0' (so that it is never counted again, you are removin
 Call the DFS in all 4 directions
 Essentially, whenever you find an island, you remove it.
 */
-class numIslandsSolution {
+class numIslandsDFSSolution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int x = grid.size(), y = 0, numIslands = 0;
@@ -305,5 +305,47 @@ private:
         for(int i = 0; i < 4; i++)  
             findIsland(a + changeX[i], b + changeY[i], grid);
       
+    }
+};
+
+/*
+A similar approach can be used for BFS (removing islands by setting elements to '0'). The code for BFS is below:
+*/
+class numIslandsBFSSolution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int x = grid.size(), y = 0, numIslands = 0;
+        if(x) y = grid[0].size();
+        
+        // iterate through grid 
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                if(grid[i][j] == '1'){
+                    numIslands++;
+                    
+                    // bfs
+                    queue<pair<int,int>> q;
+                    q.push(make_pair(i,j));
+                    
+                    while(!q.empty()){
+                        pair<int,int> top = q.front();
+                        q.pop();
+                        int a = top.first; int b = top.second;
+                        
+                        // if out of bounds or not equal to 1
+                        if( a < 0 || b < 0 || a >= x || b >= y || grid[a][b] != '1') 
+                            continue;
+                        else{
+                            // set current position to '0' and add all 4 directions to queue
+                            grid[a][b] = '0';
+                            int changeX[4] = {0,-1,1,0}, changeY[4] = {-1,0,0,1};
+                            for(int i = 0; i < 4; i++)  
+                                q.push(make_pair(a + changeX[i], b + changeY[i]));
+                        }
+                    }
+                }
+            }
+        }
+        return numIslands;
     }
 };
