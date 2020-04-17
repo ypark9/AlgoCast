a17e45c6164fa3345dc7f2e49d938d274b8c0b6b
Yoonsoo Park <ypark9@ncsu.edu>
2020-04-17 15:50:48 -0400

    numIslands

--- leetcode.cpp
+++ leetcode.cpp
@@ -261,3 +261,37 @@ int lastStoneWeight(vector<int>& stones) {
       return pq.empty() ? 0 : pq.top();  
     }
 };
+
+class numIslandsSolution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int x = grid.size(), y = 0, numIslands = 0;
+        if(x) y = grid[0].size();
+        
+        // iterate through grid and call findIsland when grid[i][j] = '1'
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                if(grid[i][j] == '1'){
+                    findIsland(i, j, grid);
+                    numIslands++;
+                }
+            }
+        }
+        return numIslands;
+    }
+private:
+    void findIsland(int a, int b, vector<vector<char>>& grid) {
+        int x = grid.size(); int y = grid[0].size();
+        
+        // if out of bounds or not '1', return
+        if( a < 0 || b < 0 || a >= x || b >= y || grid[a][b] != '1') return;
+        
+        // set the '1' to be '0' so not recounted
+        grid[a][b] = '0';
+        
+        // recurse in all 4 directions
+        int changeX[4] = {0,-1,1,0}, changeY[4] = {-1,0,0,1};
+        for(int i = 0; i < 4; i++)  findIsland(a + changeX[i], b + changeY[i], grid);
+        
+    }
+};
